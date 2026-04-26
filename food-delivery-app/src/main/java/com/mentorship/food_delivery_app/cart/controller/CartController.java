13a128c0dfde5f983bf77f197b294d8fc63b116e
amrElhady11 <amr.elhady.programs@gmail.com>
2026-04-26 21:52:12 +0300

    expose create cart endpoint in CartController

---
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/cart/controller/CartController.java
@@ -0,0 +1,29 @@
+package com.mentorship.food_delivery_app.cart.controller;
+
+import com.mentorship.food_delivery_app.cart.dto.CreateCartRequest;
+import com.mentorship.food_delivery_app.cart.dto.CreateCartResponse;
+import com.mentorship.food_delivery_app.cart.service.ICartService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/cart")
+public class CartController {
+    private final ICartService cartService;
+
+
+
+    @PostMapping("/create")
+    public ResponseEntity<CreateCartResponse> creatCart(@Valid @RequestBody CreateCartRequest request){
+
+            return new ResponseEntity<>(cartService.createCart(request), HttpStatus.CREATED);
+
+        }
+
+    }
