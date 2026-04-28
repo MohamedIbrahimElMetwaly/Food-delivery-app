74bd11283700237edebef9bd198b9f6b8c5a60af
amrElhady11 <amr.elhady.programs@gmail.com>
2026-04-28 03:03:55 +0300

    expose clear cart endpoint in CartController

--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/cart/controller/CartController.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/cart/controller/CartController.java
@@ -1,18 +1,13 @@
 package com.mentorship.food_delivery_app.cart.controller;
 
-import com.mentorship.food_delivery_app.cart.dto.AddCartItemRequest;
-import com.mentorship.food_delivery_app.cart.dto.AddToCartResponse;
-import com.mentorship.food_delivery_app.cart.dto.CreateCartRequest;
-import com.mentorship.food_delivery_app.cart.dto.CreateCartResponse;
+import com.mentorship.food_delivery_app.cart.dto.*;
 import com.mentorship.food_delivery_app.cart.service.ICartService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/cart")
@@ -31,6 +26,13 @@ public class CartController {
     @PostMapping("/cart/items")
     public ResponseEntity<AddToCartResponse> addItemToCart(@Valid @RequestBody AddCartItemRequest request)  {
             return new ResponseEntity<>(cartService.addItemToCart(request),HttpStatus.OK);
+        }
+
+        @DeleteMapping
+        public ResponseEntity<String> clearCart(@Valid @RequestBody ClearCartRequest request)  {
+            cartService.clearCart(request);
+            return ResponseEntity.noContent().build();
+
         }
 
     }
